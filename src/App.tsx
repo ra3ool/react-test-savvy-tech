35d1e971afbe4966c72076da2bb58157f6705c4f
Rasool <rasoolol.77@gmail.com>
2025-10-28 17:00:32 +0330

    feat: install lucide-react and required shadcn components

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,13 @@
+import { Button } from '@/components/ui/button';
+import { CheckCheck } from 'lucide-react';
+
 function App() {
   return (
-    <>
-      <h1 className="text-3xl font-bold underline">Hello world!</h1>
-    </>
+    <div className="flex min-h-svh flex-col items-center justify-center">
+      <Button>
+        <CheckCheck />
+      </Button>
+    </div>
   );
 }
 
